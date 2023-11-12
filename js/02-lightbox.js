09e0ed3391805d7f39ca8e8a8068cc960321c157
cefariron <adidaswww17@gmail.com>
2023-11-12 21:06:37 +0200

    complete task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const listEl = document.querySelector(".gallery")
+
+const markup = galleryItems.map(({ description, original, preview }) => 
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${original}" target="_blank">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  </li>
+`).join("")
+
+listEl.insertAdjacentHTML("beforeend", markup)
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
+
+lightbox.show();
